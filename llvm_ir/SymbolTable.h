b63b06b13490f55d3e4b39b827217c715da4bc61
Sanjana Sunil <sanjana99sunil@gmail.com>
2020-11-13 19:09:41 +0530

    added basic data structures for llvm ir generation

---
+++ llvm_ir/SymbolTable.h
@@ -0,0 +1,236 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+enum ScopeType{Function, Block, Loop};
+
+struct VariableDetails
+{
+    NodeType var_type;
+    int dimensions; // -1 if it's a function
+    vector<NodeType> argument_types; // Only for functions
+    vector<int> argument_dims;
+};
+
+class Scope
+{
+public:
+    ScopeType scope_type;
+    NodeType return_type;
+    unordered_map<string, VariableDetails> var_map; 
+
+    Scope(ScopeType scope_type, NodeType return_type) 
+        : scope_type(scope_type), return_type(return_type) {}
+    
+    bool existsInScope(string id) {
+        if(var_map.find(id) != var_map.end()) return true;
+        return false;
+    }
+
+    void redefineError(string id) {
+        error(id + " has already been defined");
+    }
+
+    void addFunction(string id, NodeType ret_type, vector<NodeType> args, vector<int> dims) {
+        if(existsInScope(id)) redefineError(id);
+        var_map[id] = {ret_type, -1, args, dims};
+    }
+
+    void addVariable(string id, NodeType var_type, int dims) {
+        if(existsInScope(id)) redefineError(id);
+        var_map[id] = {var_type, dims, {}, {}};
+    }
+
+    VariableDetails getDetails(string id) {
+        return var_map[id];
+    }
+
+    NodeType getType(string id) {
+        return var_map[id].var_type;
+    }
+
+    int getDimensions(string id) {
+        return var_map[id].dimensions;
+    }
+
+    vector<NodeType> getArgs(string id) {
+        return var_map[id].argument_types;
+    }
+
+    vector<int> getDims(string id) {
+        return var_map[id].argument_dims;
+    }
+
+    bool isFunction(string id) {
+        if(var_map[id].dimensions == -1) return true;
+        return false;
+    }
+
+    bool isValidFunctionCall(string id, vector<NodeType>& args, vector<int>& dims) {
+        if(!existsInScope(id)) return false;
+        if(!isFunction(id)) return false;
+
+        vector<NodeType> func_args = getArgs(id);
+        vector<int> func_dims = getDims(id);
+        if(args.size() != func_args.size()) return false;
+        for(int i=0; i<int(args.size()); ++i)
+        {
+            if(func_args[i] != args[i] || func_dims[i] != dims[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    bool isValidVariable(string id, int dimensions) {
+        if(!existsInScope(id)) return false;
+        if(dimensions != getDimensions(id)) return false;
+
+        return true;
+    }
+};
+
+class SymbolTable
+{
+public:
+    vector<Scope*> scopes;
+
+    void addBlockScope() {
+        Scope *block_scope = new Scope(Block, NONE);
+        scopes.push_back(block_scope);
+    }
+
+    void addLoopScope() {
+        Scope *loop_scope = new Scope(Loop, NONE);
+        scopes.push_back(loop_scope);
+    }
+
+    void addFunctionScope(string return_type) {
+        NodeType ret_type = getNodeType(return_type);
+        Scope *func_scope = new Scope(Function, ret_type);
+        scopes.push_back(func_scope);
+    }
+
+    void addFunctionToCurrentScope(string id, string return_type, vector<string>& arguments, vector<int>& dims) {
+        NodeType ret_type = getNodeType(return_type);
+
+        vector<NodeType> args;
+        for(string argument : arguments) args.push_back(getNodeType(argument));
+
+        Scope *recent_scope = scopes.back();
+        recent_scope->addFunction(id, ret_type, args, dims);
+    }
+
+    void addVariableToCurrentScope(string id, string variable_type, int dims) {
+        NodeType var_type = getNodeType(variable_type);
+        Scope *recent_scope = scopes.back();
+        recent_scope->addVariable(id, var_type, dims);
+    }
+
+    VariableDetails getDetails(string id) {
+        int n = scopes.size();
+        for(int i=n-1; i>=0; --i)
+        {
+            if(scopes[i]->existsInScope(id))
+                return scopes[i]->getDetails(id);
+        }
+
+        error("Invalid reference");
+    }
+
+    NodeType getType(string id) {
+        int n = scopes.size();
+        for(int i=n-1; i>=0; --i)
+        {
+            if(scopes[i]->existsInScope(id))
+                return scopes[i]->getType(id);
+        }
+
+        return NONE;
+        // error("Invalid reference");
+    }
+
+    int getDimensions(string id) {
+        int n = scopes.size();
+        for(int i=n-1; i>=0; --i)
+        {
+            if(scopes[i]->existsInScope(id))
+                return scopes[i]->getDimensions(id);
+        }
+
+        error("Invalid reference");
+    }
+
+    vector<NodeType> getArgs(string id) {
+        int n = scopes.size();
+        for(int i=n-1; i>=0; --i)
+        {
+            if(scopes[i]->existsInScope(id))
+                return scopes[i]->getArgs(id);
+        }
+
+        error("Invalid reference");
+    }
+
+    vector<int> getDims(string id) {
+        int n = scopes.size();
+        for(int i=n-1; i>=0; --i)
+        {
+            if(scopes[i]->existsInScope(id))
+                return scopes[i]->getDims(id);
+        }
+
+        error("Invalid reference");
+    }
+
+    bool existsInCurrentScope(string id) {
+        return (scopes.back())->existsInScope(id);
+    }
+
+    bool isValidFunctionCall(string id, vector<NodeType> args, vector<int> dims) {
+        int n = scopes.size();
+        for(int i=n-1; i>=0; --i)
+        {
+            if(scopes[i]->isValidFunctionCall(id, args, dims))
+                return true;
+        }
+        return false;
+    }
+
+    bool isValidVariable(string id, int dimensions) {
+        int n = scopes.size();
+        for(int i=n-1; i>=0; --i)
+        {
+            if(scopes[i]->isValidVariable(id, dimensions))
+                return true;
+        }
+        return false;
+    }
+
+    NodeType getCurrentReturnType() {
+        for(int i=0; i<int(scopes.size()); ++i)
+        {
+            if(scopes[i]->scope_type == Function)
+                return scopes[i]->return_type;
+        }
+        return NONE;
+    }
+
+    bool scopeTypeExists(ScopeType scope_type) {
+        for(int i=0; i<int(scopes.size()); ++i)
+        {
+            if(scopes[i]->scope_type == scope_type)
+                return true;
+        }
+        return false;
+    }
+
+    void removeScope() {
+        Scope* recent_scope = scopes.back();
+        scopes.pop_back();
+        delete recent_scope;
+    }
+};
---
+++ llvm_ir/ast.h
@@ -0,0 +1,539 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class ASTProg;
+class ASTProgStat;
+
+class ASTBlockStat;
+
+class ASTVariable;
+class ASTVariableAssign;
+class ASTVariableDecl;
+class ASTVariableDeclType;
+
+class ASTFuncArg;
+class ASTFuncDecl;
+class ASTFuncCall;
+
+class ASTExpr;
+class ASTExprUnary;
+class ASTExprBinary;
+class ASTExprTernary;
+class ASTExprInt;
+class ASTExprFloat;
+class ASTExprChar;
+class ASTExprBool;
+class ASTExprString;
+class ASTExprVar;
+class ASTExprFuncCall;
+
+class ASTStat;
+class ASTStatVarAssign;
+class ASTStatVarDecl;
+class ASTStatBlock;
+class ASTStatFuncCall;
+class ASTStatReturn;
+class ASTStatLoopControl;
+class ASTStatWhile;
+class ASTStatIf;
+class ASTStatFor;
+
+class ASTvisitor
+{
+public:
+    virtual llvm::Value* visit(ASTProg &node) = 0;
+    virtual llvm::Value* visit(ASTProgStat &node) = 0;
+    virtual llvm::Value* visit(ASTBlockStat &node) {};
+    virtual llvm::Value* visit(ASTVariable &node) {};
+    virtual llvm::Value* visit(ASTVariableAssign &node) {};
+    virtual llvm::Value* visit(ASTVariableDecl &node) {};
+    virtual llvm::Value* visit(ASTVariableDeclType &node) {};
+    virtual llvm::Value* visit(ASTFuncArg &node) {};
+    virtual llvm::Value* visit(ASTFuncDecl &node) {};
+    virtual llvm::Value* visit(ASTFuncCall &node) {};
+
+    virtual llvm::Value* visit(ASTExprUnary &node) {};
+    virtual llvm::Value* visit(ASTExprBinary &node) {};
+    virtual llvm::Value* visit(ASTExprTernary &node) {};
+    virtual llvm::Value* visit(ASTExprInt &node) {};
+    virtual llvm::Value* visit(ASTExprFloat &node) {};
+    virtual llvm::Value* visit(ASTExprChar &node) {};
+    virtual llvm::Value* visit(ASTExprBool &node) {};
+    virtual llvm::Value* visit(ASTExprString &node) {};
+    virtual llvm::Value* visit(ASTExprVar &node) {};
+    virtual llvm::Value* visit(ASTExprFuncCall &node) {};
+    virtual llvm::Value* visit(ASTExpr &node) {};
+
+    virtual llvm::Value* visit(ASTStatFor &node) {};
+    virtual llvm::Value* visit(ASTStatIf &node) {};
+    virtual llvm::Value* visit(ASTStatWhile &node) {};
+    virtual llvm::Value* visit(ASTStatLoopControl &node) {};
+    virtual llvm::Value* visit(ASTStatReturn &node) {};
+    virtual llvm::Value* visit(ASTStatFuncCall &node) {};
+    virtual llvm::Value* visit(ASTStatVarDecl &node) {};
+    virtual llvm::Value* visit(ASTStatVarAssign &node) {};
+    virtual llvm::Value* visit(ASTStat &node) {};
+};
+
+class ASTnode
+{
+public:
+    NodeType node_type;
+    virtual ~ASTnode()
+    {
+    }
+
+    virtual llvm::Value* accept(ASTvisitor &V) = 0;
+};
+
+class ASTProg : public ASTnode
+{
+public:
+    std::vector<ASTProgStat *> progStatList;
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTProgStat : public ASTnode
+{
+public:
+    ASTVariableDecl *var_decl;
+    ASTFuncDecl *func_decl;
+
+    ASTProgStat(ASTVariableDecl *_var_decl, ASTFuncDecl *_func_decl) : var_decl(_var_decl), func_decl(_func_decl) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTBlockStat : public ASTnode
+{
+public:
+    std::vector<ASTStat *> statementList;
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTVariable : public ASTnode
+{
+public:
+    string id;
+    ASTExpr *param1;
+    ASTExpr *param2;
+
+    ASTVariable(string id, ASTExpr *_param1, ASTExpr *_param2) : id(id), param1(_param1), param2(_param2) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+
+    int getDimensions() {
+        if(!param1) return 0;
+        if(!param2) return 1;
+        return 2;
+    }
+};
+
+class ASTVariableAssign : public ASTnode
+{
+public:
+    string id;
+    ASTExpr *exp;
+
+    ASTVariableAssign(string id, ASTExpr *_exp) : id(id), exp(_exp) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTVariableDecl : public ASTnode
+{
+public:
+    string lit_type;
+
+    vector<ASTVariableDeclType*> varList;
+    ASTVariableDecl(string lit_type) : lit_type(lit_type) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTVariableDeclType : public ASTnode
+{
+public:
+    ASTVariable* var;
+    ASTVariableAssign* var_assign;
+
+    ASTVariableDeclType(ASTVariable* var, ASTVariableAssign* var_assign) : var(var), var_assign(var_assign) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+
+    string getId() {
+        if(var) return var->id;
+        return var_assign->id;
+    }
+};
+
+// Func
+
+class ASTFuncArg : public ASTnode
+{
+public:
+    string lit_type;
+    string id;
+    int dimension;
+
+    ASTFuncArg(string lit_type, string id, int dimension) : lit_type(lit_type), id(id), dimension(dimension) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTFuncDecl : public ASTnode
+{
+public:
+    string lit_type;
+    string id;
+    ASTBlockStat* block;
+
+    vector<ASTFuncArg*> funcArgList;
+
+    ASTFuncDecl(string lit_type, string id, ASTBlockStat *_block) : lit_type(lit_type), id(id), block(_block) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTFuncCall : public ASTnode
+{
+public:
+    string id;
+
+    vector<ASTExpr*> funcArgList;
+    ASTFuncCall(string id) : id(id) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+
+// Expr
+class ASTExpr : public ASTnode
+{
+public:
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprUnary : public ASTExpr
+{
+public:
+    string unary_op;
+    ASTExpr *exp;
+
+    ASTExprUnary(string unary_op, ASTExpr *_exp) : unary_op(unary_op), exp(_exp) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprBinary : public ASTExpr
+{
+public:
+    string bin_operator;
+    ASTExpr *left;
+    ASTExpr *right;
+
+    ASTExprBinary(std::string op, ASTExpr *_left, ASTExpr *_right) : bin_operator(op), left(_left), right(_right) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprTernary : public ASTExpr
+{
+public:
+    ASTExpr *first;
+    ASTExpr *second;
+    ASTExpr *third;
+
+    ASTExprTernary(ASTExpr *_first, ASTExpr *_second, ASTExpr *_third) : first(_first), second(_second), third(_third) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprInt : public ASTExpr
+{
+public:
+    int intlit;
+
+    ASTExprInt(int intlit) : intlit(intlit) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprFloat : public ASTExpr
+{
+public:
+    float floatlit;
+
+    ASTExprFloat(float floatlit) : floatlit(floatlit) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprChar : public ASTExpr
+{
+public:
+    char charlit;
+
+    ASTExprChar(char charlit) : charlit(charlit) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprBool : public ASTExpr
+{
+public:
+    bool boollit;
+
+    ASTExprBool(bool boollit) : boollit(boollit) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprString : public ASTExpr
+{
+public:
+    string stringlit;
+
+    ASTExprString(string stringlit) : stringlit(stringlit) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprVar : public ASTExpr
+{
+public:
+    ASTVariable *var;
+
+    ASTExprVar(ASTVariable *var) : var(var) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTExprFuncCall : public ASTExpr
+{
+public:
+    ASTFuncCall *func_call;
+
+    ASTExprFuncCall(ASTFuncCall *_func_call) : func_call(_func_call) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+
+// Stat
+class ASTStat : public ASTnode
+{
+public:
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTStatVarAssign : public ASTStat
+{
+public:
+    ASTVariable *var;
+    ASTExpr *exp;
+
+    ASTStatVarAssign(ASTVariable *_var, ASTExpr *_exp) : var(_var), exp(_exp) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTStatVarDecl : public ASTStat
+{
+public:
+    ASTVariableDecl *var_decl;
+
+    ASTStatVarDecl(ASTVariableDecl *_var_decl) : var_decl(_var_decl) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTStatBlock : public ASTStat
+{
+public:
+    ASTBlockStat *block;
+
+    ASTStatBlock(ASTBlockStat *_block) : block(_block) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTStatFuncCall : public ASTStat
+{
+public:
+    ASTFuncCall *func_call;
+
+    ASTStatFuncCall(ASTFuncCall *_func_call) : func_call(_func_call) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTStatReturn : public ASTStat
+{
+public:
+    ASTExpr *return_expr;
+
+    ASTStatReturn(ASTExpr *_return_expr) : return_expr(_return_expr) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTStatLoopControl: public ASTStat
+{
+public:
+    string control_stat;
+
+    ASTStatLoopControl(string control_stat) : control_stat(control_stat) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTStatWhile : public ASTStat
+{
+public:
+    ASTExpr *exp;
+    ASTBlockStat *block;
+
+    ASTStatWhile(ASTExpr *_exp, ASTBlockStat *_block) : exp(_exp), block(_block) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTStatIf : public ASTStat
+{
+public:
+    vector<ASTExpr *> exprList;
+    vector<ASTBlockStat *> blockList;
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTStatFor : public ASTStat
+{
+public:
+    ASTVariableDecl *var_decl;
+    ASTVariable *init_var;
+    ASTExpr *init_expr;
+    ASTExpr *cond_expr;
+    ASTVariable *loop_var;
+    ASTExpr *loop_expr;
+    ASTBlockStat *block;
+
+    ASTStatFor(ASTVariableDecl *var_decl, ASTVariable *init_var, ASTExpr *init_expr, ASTExpr *cond_expr, ASTVariable *loop_var, ASTExpr *loop_expr, ASTBlockStat *block) 
+        : var_decl(var_decl), init_var(init_var), init_expr(init_expr), cond_expr(cond_expr), loop_var(loop_var), loop_expr(loop_expr), block(block) {}
+
+    virtual llvm::Value* accept(ASTvisitor &v)
+    {
+        v.visit(*this);
+    }
+};
+
+class ASTContext
+{
+public:
+    ASTnode *root;
+
+    ~ASTContext()
+    {
+        clearAST();
+    }
+
+    /// free all saved expression trees
+    llvm::Value* clearAST()
+    {
+        delete root;
+    }
+};
